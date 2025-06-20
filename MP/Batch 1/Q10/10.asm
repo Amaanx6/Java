c54bbac216292b3cd61f1fd14e476b2351e9d946
Amaanx6 <mubashiruddinmd@gmail.com>
2025-06-21 00:21:51 +0530

    Q10

---
+++ MP/Batch 1/Q10/10.asm
@@ -0,0 +1,16 @@
+Exp 10: CODE CONVERSION –DECIMAL TO HEXDECIMAL 
+
+AIM: 
+To convert a given decimal number to hexadecimal. 
+
+ALGORITHM: 
+1. Initialize the memory location to the data pointer. 
+2. Increment B register. 
+3. Increment accumulator by 1 and adjust it to decimal every time. 
+4. Compare the given decimal number with accumulator value. 
+5. When both matches , the equivalent hexadecimal value is in B register. 
+6. Store the resultant in memory location. 
+
+RESULT: 
+Thus an ALP program for conversion of decimal to hexadecimal was written and 
+executed.
