96b8d279d286d3535e181ccdc1add9047a8d669f
Amaanx6 <mubashiruddinmd@gmail.com>
2025-06-20 23:47:22 +0530

    MP 5q

---
+++ MP/5/5.asm
@@ -0,0 +1,16 @@
+Exp 5: 8 BIT DATA DIVISION 
+AIM: 
+To divide two 8-bit numbers and store the result in memory. 
+
+ALGORITHM: 
+LOGIC: Division is done using the method Repeated subtraction. 
+1. Load Divisor and Dividend 
+2. Subtract divisor from dividend 
+3. Count the number of times of subtraction which equals the quotient 
+4. Stop subtraction when the dividend is less than the divisor .The dividend now 
+becomes the remainder. Otherwise go to step 2. 
+5. stop the program execution. 
+
+RESULT: 
+Thus an ALP was written for 8-bit division using repeated subtraction method and 
+executed using 8085 p kits
