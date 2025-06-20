4690f00542e09add4958920ddb5a3bc5facde380
Amaanx6 <mubashiruddinmd@gmail.com>
2025-06-21 00:29:49 +0530

    Q12

---
+++ MP/Batch 1/Q12/Q12.asm
@@ -0,0 +1,15 @@
+Exp 12: BCD ADDITION 
+
+AIM: 
+To add two 8 bit BCD numbers stored at consecutive memory locations. 
+
+ALGORITHM: 
+1. Initialize memory pointer to data location. 
+2. Get the first number from memory in accumulator. 
+3. Get the second number and add it to the accumulator 
+4. Adjust the accumulator value to the proper BCD value using DAA instruction. 
+5. Store the answer at another memory location. 
+
+RESULT: 
+Thus the 8 bit BCD numbers stored at 4500 &4501 are added and the result stored at 4502& 
+4503.
