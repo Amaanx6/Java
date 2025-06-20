c85598c838631265c495ef708160d3ed26820812
Amaanx6 <mubashiruddinmd@gmail.com>
2025-06-21 00:26:32 +0530

    Q2

---
+++ MP/Batch 1/Q2/Q2.asm
@@ -0,0 +1,14 @@
+Exp 2. 8 BIT DATA ADDITION 
+
+AIM: 
+To add two 8 bit numbers stored at consecutive memory locations. 
+
+ALGORITHM: 
+1. Initialize memory pointer 
+2. to data location. 
+3. Get the first number from memory in accumulator. 
+4. Get the second number and add it to the accumulator. 
+5. Store the answer at another memory location. 
+
+RESULT: 
+Thus the 8 bit numbers stored at 4500 &4501 are added and the result stored at 4502 &4503.
