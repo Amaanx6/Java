0edd12d6ce58562f0525857dc0d8f82c255d96d4
Amaanx6 <mubashiruddinmd@gmail.com>
2025-06-23 23:23:17 +0530

    OS Q1

---
+++ OS/Q1.c
@@ -0,0 +1,24 @@
+// 🗂️ A) Main File Commands 
+// Command	         Use
+// ls	     List files in a directory
+// cd	     Change current directory
+// pwd	     Show current directory path
+// touch	 Create an empty file
+// mkdir	 Create a new directory
+// rm	     Delete files or directories
+// cp	     Copy files or directories
+// mv	     Move or rename files
+// cat	     Display file contents
+// chmod	 Change file permissions
+// chown	 Change file ownership
+
+// 🧠 B) Main Process Commands 
+// Command	      Use
+// ps	     Show running processes
+// top	     Real-time process viewer
+// kill	     Stop a process using PID
+// pkill	 Stop a process by name
+// jobs	     List background jobs
+// fg	     Bring job to foreground
+// bg	     Resume job in background
+// &	     Run command in background
